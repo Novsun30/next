d24756a19d44bd9d7c0adaa38cfd8ffdfece5766
novsun30 <novsun30@gmail.com>
2024-01-22 16:22:47 +0800

    update eslint config and enforcing the style of code

--- app/todo/layout.tsx
+++ app/todo/layout.tsx
@@ -5,13 +5,13 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "To do list page",
-	description: "Just a to do list",
+	description: "Just a to do list"
 };
 
-export default function TodoLayout({
-	children,
+export default function TodoLayout ({
+	children
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
 	return (
 		<html lang="en">
